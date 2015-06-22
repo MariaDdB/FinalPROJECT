da03f894a779a8abc68a6ea4f31bd3a8ab78240a
MariaDdB <mcdbcores@gmail.com>
2015-06-22 17:24:25 +0200

    jquery-ui/dad DONE

--- Move_your_Bones/app/assets/javascripts/skeletons.js
+++ Move_your_Bones/app/assets/javascripts/skeletons.js
@@ -1,29 +1,35 @@
-var correctBones = 0;
-
 $(init);
 
-/*	//Reset the game
-	correctBones = 0;
-	$('.draggable').html('');
-	$('#droppable').html('');
+var count = 0;
 
-	$('.draggable').each(function(){
-		console.log($(this))
-	});
-*/
-	function init () {
-		$('.draggable').draggable ({
-			contaiment: '#content',
+var bonesLength = $('.draggable').size();
+
+console.log(bonesLength);
+
+function init(){
+	for(var i = 0; i<bonesLength; i++){
+		$('#bone'+i).data('bone', i).draggable({
 			cursor: 'move',
 			revert: 'invalid'
 		});
+	}
+}
 
-		$('#droppable').droppable ({
+	for(var i = 0; i<bonesLength; i++){	
+		$("<div></div>").data('bone', i).attr('id', 'match'+i).appendTo('#match').droppable({
+			accept: '#bone'+i,
+			hoverClass: 'hovered',
 			drop: handleDropEvent
 		});
 	}
 
-	function handleDropEvent (event, ui) {
-		var draggable = ui.draggable;
-		alert ('The bone was dropped!'); 
-	}
+function handleDropEvent(event, ui){
+	var slotBone = $(this).data('bone');
+	var boneMatch = ui.draggable.data('bone');
+
+	if (slotBone == boneMatch) {
+		ui.draggable.draggable('disable');
+		$(this).droppable('disable');
+		ui.draggable.draggable('option', 'revert', false);
+	}	
+}
